e92bf4d04be625f685424169f945c847eb360130
zhouhong1217 <zhouhong1217@163.com>
2017-07-21 15:16:37 +0800

    修改

---
+++ indexJs.js
@@ -0,0 +1,15 @@
+/**
+ * Created by zh on 17-7-21.
+ */
+// let str = 'abcdefadvsd';
+// let char = 100;
+// function findNum(str,char) {
+//     let num = 0;
+//     for(let i = 0;i < str.length;i++){
+//         if(str.charCodeAt(i) == char){
+//             num ++;
+//         }
+//     }
+//     return num;
+// }
+// console.log(findNum(str,char))
